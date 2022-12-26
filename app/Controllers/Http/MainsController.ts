2f46548e1d57e9d8be5ef3e404522b6f2ec8d1bc
Daniel Assani <devasddaniel@gmail.com>
2022-12-26 11:18:17 +0200

    add data

--- app/Controllers/Http/MainsController.ts
+++ app/Controllers/Http/MainsController.ts
@@ -1,9 +1,11 @@
  import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Article from 'App/Models/Article'
+
 
 export default class MainsController {
     async index({view}:HttpContextContract){
-
-      return view.render("blog/index")
+      let articles = await Article.all()
+      return view.render("blog/index", {articles})
     }
 
     async article({view}:HttpContextContract){
--- app/Models/Article.ts
+++ app/Models/Article.ts
@@ -4,10 +4,21 @@ import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 export default class Article extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+  @column()
+  public title:string
+
+  @column()
+  public content:string
+
+  @column()
+  public online:boolean
+
+  @column()
+  public thumbnail:string|null
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime|null
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime|null
 }
